453a9d796232f31c2e438781dfa7170e26494a37
rosamontalvoy <montalvoyebenes.rosa@gmail.com>
2023-06-19 18:01:55 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,12 +7,13 @@ app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
+    path('navbar/', views.my_view, name='navbar'),
     # name the URL
 
     # path for about view
-
+    path('about/', views.about_view, name='about'),
     # path for contact us view
-
+    path('contact/', views.contact_view, name='contact'),
     # path for registration
 
     # path for login
